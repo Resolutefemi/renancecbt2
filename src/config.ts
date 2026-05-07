7021741da060ebda1f6addc98b5920805d7c42bf
rustbymastery <ariyooluwafemi487@gmail.com>
2026-05-07 20:26:38 +0100

    Revert "conversion to ts"
    This reverts commit ac10c6a10be4e31aefeb17493a9337d9af7200a8.

--- src/config.ts
+++
@@ -1,5 +0,0 @@
-
-export const SUPABASE_CONFIG = {
-    URL: 'https://ubxsywaxdvkhiqepcvmq.supabase.co',
-    KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVieHN5d2F4ZHZraGlxZXBjdm1xIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE5ODcwMDAsImV4cCI6MjA4NzU2MzAwMH0.gkR3Aud3LRLNyNwpDHJTT0vIrWCnQkSBkFSaFjQ5qy4'
-};
