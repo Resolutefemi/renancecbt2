f42ad1f699874edbe98bc4da6aaeef2076bc9fd7
rustbymastery <ariyooluwafemi487@gmail.com>
2026-05-07 18:34:25 +0100

    min commit

---
+++ src/about.ts
@@ -0,0 +1,12 @@
+
+/**
+ * about.ts - Simple logic for the About page
+ */
+
+declare const lucide: any;
+
+window.addEventListener('DOMContentLoaded', () => {
+    if (typeof lucide !== 'undefined') {
+        lucide.createIcons();
+    }
+});
