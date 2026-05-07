b910ae95cb234d56c029afad6a68eee6647c3031
rustbymastery <ariyooluwafemi487@gmail.com>
2026-05-07 18:27:58 +0100

    course updt

--- src/cos101.ts
+++ src/cos101.ts
@@ -1,10 +1,8 @@
 
 import { QuizEngine } from './quizEngine';
+import { SUPABASE_CONFIG } from './config';
 
-const SUPABASE_URL = 'https://ubxsywaxdvkhiqepcvmq.supabase.co';
-const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVieHN5d2F4ZHZraGlxZXBjdm1xIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE5ODcwMDAsImV4cCI6MjA4NzU2MzAwMH0.gkR3Aud3LRLNyNwpDHJTT0vIrWCnQkSBkFSaFjQ5qy4';
-
-const engine = new QuizEngine('COS 101', SUPABASE_URL, SUPABASE_KEY);
+const engine = new QuizEngine('COS 101', SUPABASE_CONFIG.URL, SUPABASE_CONFIG.KEY);
 
 document.addEventListener('DOMContentLoaded', () => {
     engine.init('questions/COS101.json');
